606589aac593de62546786e628fe40fc7fc78d61
Matt States <mattstates@icloud.com>
2016-04-05 16:04:32 -0700

    functional program

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-/*app.get('/', function(req, res) {
-    res.send('Hello World');
-})*/
-
 app.get('/:timestamp', function(req, res) {
-   //var test = req.query('timestamp');
-    res.send(req.params.timestamp + 'this is what you sent.') 
+    
+    var time = new Date(req.params.timestamp);
+    time = time.toUTCString(); //output EX: "Sat, 10 Feb 2007 08:00:00 GMT"
+    
+    var utcTime = new Date();
+    utcTime = utcTime.setTime(req.params.timestamp); //output EX: 1199347200000
+   
+    if(time === 'Invalid Date') {
+        time = new Date(utcTime);
+        time = time.toUTCString();
+    }
+    
+    if(!utcTime) {
+        utcTime = Date.parse(time);
+    }
+    
+    var output = {
+        'Time': time,
+        'UTC': utcTime
+    }
+    
+    res.send(output);
 })
 
 app.listen(port, function() {
